69c5f7801e2480be88ef77bf386d757f827d0bac
Francesco <francsco8bassino@gmail.com>
2021-02-04 13:01:37 -0500

    Ordering code

--- Drawing Fractals/main.cpp
+++ Drawing Fractals/main.cpp
@@ -1,76 +1,26 @@
 #include <iostream>
-#include <cstdint>
-#include <memory>
-#include <math.h>
-#include "Bitmap.h"
-#include "Mandelbrot.h"
-#include "ZoomList.h"
 #include "FractalCreator.h"
 
-using namespace std;
 using namespace franyNamespace;
-
-signed int const WIDTH = 800;
-signed int const HEIGHT = 600;
 
 
 int main() {
+
+	int const m_height = 600;
 	
-	cout << "Creating the Fractal..." << endl;
+	std::cout << "Creating the Fractal..." << std::endl;
 
-	Bitmap bitmap(WIDTH, HEIGHT);
+	FractalCreator fractalCreator(800, 600);
 
-	ZoomList zoomList(WIDTH, HEIGHT);
-	zoomList.add(Zoom(WIDTH / 2, HEIGHT / 2, 4.0/WIDTH));
-	zoomList.add(Zoom(295, HEIGHT - 202, 0.1));
-	zoomList.add(Zoom(312, HEIGHT - 304, 0.1));
+	fractalCreator.addZoom(Zoom(295, m_height - 202, 0.1));
+	fractalCreator.addZoom(Zoom(312, m_height - 304, 0.1));
 
-	double min = 999999;
-	double max = -999999;
+	fractalCreator.calculateIteration();
+	fractalCreator.calculateTotalIterations();
+	fractalCreator.drawFractal();
 
-	unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS + 1]{});
-	unique_ptr<int[]> fractal(new int[WIDTH*HEIGHT]{});
-	
-	for (int y = 0; y < HEIGHT; ++y) {
-		for (int x = 0; x < WIDTH; ++x) {
-			pair<double, double> coords = zoomList.doZoom(x, y);
+	fractalCreator.writeBitmap("test.bmp");
 
-			int iterations = Mandelbrot::getIterations(coords.first, coords.second);
-			fractal[y * WIDTH + x] = iterations;
-
-			if(iterations != Mandelbrot::MAX_ITERATIONS)
-				histogram[iterations]++;
-		}
-	}
-
-	int total = 0;
-	for (int i = 0; i < Mandelbrot::MAX_ITERATIONS; ++i) {
-		total += histogram[i];
-	}
-	for (int y = 0; y < HEIGHT; y++) {
-		for (int x = 0; x < WIDTH; x++) {
-
-			uint8_t red = 0;
-			uint8_t green = 0;
-			uint8_t blue = 0;
-
-			int iterations = fractal[y * WIDTH + x];
-			if (iterations != Mandelbrot::MAX_ITERATIONS) {
-
-				double hue = 0.0;
-
-				for (int i = 0; i <= iterations; ++i) {
-					hue += ((double)histogram[i]) / total;
-				}
-				green = pow(255, hue);
-			}
-
-			bitmap.setPixel(x, y, red, green, blue);
-		}
-	}
-
-	bitmap.writeBitmap("test.bmp");
-
-	cout << "Finished." << endl;
+	std::cout << "Finished." << std::endl;
 	return 0;
 }
